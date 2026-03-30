b72da5e3329ab299a49631c0c9ce981ee2ef93de
hyejin <lmjk2100@gmail.com>
2026-03-30 15:22:37 +0900

    수정

--- array/2array-2.c
+++ array/2array-2.c
@@ -3,16 +3,14 @@
 int main(void)
 {
     int i;
-    char menu[5][10] = { //5행 10열 (0~4행, 0~9열)
-        "init", // 1행씩 초기값 설정
-        "open",
-        "close",
-        "read",
-        "write"
-    };
+    char fruit[3][20];
 
-    for (i = 0; i < 5; i++) //0~4
-        printf("%d 번째 메뉴: %s \n", i, menu[i]); //menu[i]는 행을 의미
+    for (i = 0; i < 3; i++) { //0~2
+        printf("과일 이름을 입력하세요: ");
+        scanf("%s", fruit[i]);
+        printf("%d 번째 과일은: %s \n", i, fruit[i]); //menu[i]는 행을 의미
+
+    }
 
     return 0;
 }
