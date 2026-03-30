86d582c5ddacec2495292e168607c247ea91734f
hyejin <lmjk2100@gmail.com>
2026-03-30 15:18:37 +0900

    배열 연습

---
+++ array/char-array-1.c
@@ -0,0 +1,17 @@
+﻿// 문자열의 길이를 구하는 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    char str[30] = "C language"; //문자배열 30자
+    int i = 0;
+
+    while (str[i] != '\0') //만족할때 반복
+                           // != -> 다르다, \n는 null 값을 의미함
+                           //문자열 내용 끝나는 시점에는 반드시 null값이 들어감
+        i++;
+
+    printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+    return 0;
+}
