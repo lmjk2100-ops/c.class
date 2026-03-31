4c97885b72e367c67c792f5ce5cb0e91732eaffd
hyejin <lmjk2100@gmail.com>
2026-03-31 09:15:15 +0900

    배열 교환

---
+++ array/array2.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	char s[] = "ABCD";
+	int a = 0, b = 3;
+	char t;
+	while (a < b)
+	{
+		t = s[a];
+		s[a] = s[b];
+		s[b] = t;
+		a++;
+		b--;
+	}
+
+	printf("%s\n", s);
+	return 0;
+}
