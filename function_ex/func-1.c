6e8457ba3d033d10d164a720b2a67de066fe9f4e
hyejin <lmjk2100@gmail.com>
2026-03-31 12:40:12 +0900

    함수  기초 실습

---
+++ function_ex/func-1.c
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int Add(int a, int b);
+int Input(void);
+void Result_Print(int val);
+void Intro(void);
+
+int main(){ //시작은 main부터
+		int a, b, result;
+
+		Intro(); // 함수 호출, ()안에 인수가 없음
+		//함수 앞에 형이 없음. 반환되는 것이 없음
+		a = Input(); //Input() 함수 호출, 인수없음
+		b = Input(); //Input() 함수 호출, 인수없음
+		result = Add(a, b); //Add 함수 호출, 인수 있음(정수형 a,b)
+		Result_Print(result); //Result_Print 호출, 인수 하나(정수형)
+
+		return 0;
+	}
+
+	void Intro() { //함수, ()안에 매개변수 없음
+		// 반한되는 값이 없음(void)
+		printf("****** star ****** \n"); //제목 출력 부분
+		printf("두 개의 정수 입력: \n");
+	}
+
+	int Input() { // 사용자 정의 함수, 매개변수 없음, 정수 입력 부분
+		int Input; //변수 선언
+		scanf("%d", &Input); //정수형 숫자 입력
+		return Input; // 값을 반환(보냄)
+		//int Input() -> int는 반환되는 값의 형 의미
+	}
+
+	int Add(int i, int j) { //int i, int j -> 매개변수(형 선언)
+		//a -> i, b -> j (인수와 매개변수의 이름은 같게 해도 되지만 형은 같아야함)
+		return i + j; //더한닶을 반환(int형)
+		//int Add(int i, int j) -> int
+	}
+	 
+	void Result_Print(int val) { //result -> 매개변수 val(정수형)
+		printf("덧셈에 대한 결과 : %d \n", val);
+		printf("****** END ******");
+		//반환이 없음(void0
+	}
