8b93b2b5efa79bfbcd3ad91e61b43e43fe040db8
hyejin <lmjk2100@gmail.com>
2026-04-06 14:10:47 +0900

    포인터 실습

---
+++ pointer/double-pointer.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int* numPtr1;	//단일 포인터 선언
+	int** numPtr2;	//이중 포인터 선언
+	int num1 = 10;
+
+	int* numPtr1 = &num1; //num1의 메모리 주소 저장
+	int* numPtr2 = &numPtr1; //numPtr1의 메모리 주소 저장
+	printf("%d\n", **numPtr2);
+	// 포인터를 두 번 역참조하여 num1의 메모리 주소에 접근
+	return 0;
+}
---
+++ pointer/point-5.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+int add(int x, int y);
+void sub(int i, int j);
+void arr_prn(int d[5]);
+
+int main(void)
+{
+	int a = 10;
+	int b = 20;
+	int c[5] = { 1,2,3,4,5 }; //배열
+	add(a, b);    //함수 호출, 인수 2개 -> 더할려고
+	sub(a, b);
+	printf("%d\n", add(a, b));
+
+	arr_prn(c);
+
+
+	return 0;
+}
+int add(int x, int y) //매개변수
+{
+	int k = x + y;
+
+	return k; //30 반환
+}
+void sub(int i, int j)
+{
+	int g = i - j;
+	printf("%d", g);
+}
+
+void arr_prn(int d[5])
+{
+	for (int i = 0; i < 5; i++) {
+		printf("%d\n", d[i]);
+	}
+}
